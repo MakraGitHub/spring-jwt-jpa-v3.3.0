7c71b391982e24199780d12bca67345040429324
Makara <makaragit.sam@2023.com>
2024-06-02 16:47:16 +0700

    config-entity

--- src/main/java/co/prime/jpa/repository/BookRepository.java
+++ src/main/java/co/prime/jpa/repository/BookRepository.java
@@ -1,12 +1,20 @@
 package co.prime.jpa.repository;
 
 import co.prime.jpa.entity.Book;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends CrudRepository<Book, UUID> {
+
+    List<Book> findByTitleIsContainingIgnoreCase(String title);
+    List<Book> findByTitleIsStartingWithIgnoreCase(String title);
+
+    @Query("select b from e_book b where b.author =:name")
+    List<Book> selectByAuthorName(String name);
 
 }
